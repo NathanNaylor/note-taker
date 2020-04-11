5e46a17104c8519ec7d9629ab877a59308764108
Squelchert <61394430+NathanNaylor@users.noreply.github.com>
2020-04-10 17:03:23 -0700

    fixed commented out route

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ app.get("/notes", (req, res) => {
 
 })
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "/public/index.html"))
-// })
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+})
 
 app.get("/api/notes", (req, res) => {
     res.json(getNotes());
